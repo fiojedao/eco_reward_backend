283f2364cca205e081fe6b733a76682c523b2684
fiojeda <felix@centaurosolutions.com-at-191127002560>
2023-11-13 00:05:35 -0600

    Update materialExchangeController.js

--- controllers/materialExchangeController.js
+++ controllers/materialExchangeController.js
@@ -2,12 +2,25 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 //Obtener listado
 module.exports.get = async (request, response, next) => {
-  const centers = await prisma.recycling_Material_Exchange.findMany({
+  const userId = 3; // Reemplaza 1 con el ID del usuario que estás buscando
+
+  const userExchangeHistory = await prisma.recycling_Material_Exchange.findMany({
+    where: {
+      client_userID: userId,
+    },
     orderBy: {
-      exchangeID: "asc",
+      exchange_date: "asc",
+    },
+    include: {
+      Exchange_Material_Details: {
+        include: {
+          Recycling_Material: true,
+        },
+      },
+      Recycling_Center: true,
     },
   });
-  response.json(centers);
+  response.json(userExchangeHistory);
 };
 //Obtener por Id
 module.exports.getById = async (request, response, next) => {
